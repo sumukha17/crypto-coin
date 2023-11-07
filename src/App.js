be6f59c90103aef7c2787b1d7790a3bbad14a746
Sumukha17 <Sumukhabharadwaj666@gmail.com>
2023-11-07 10:59:33 +0530

    initial project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState,useEffect } from "react";
 import Coin from "./Coin";
-import './style.css'
+import './style.css';
 
-function App() {
-  const [listOfCoins,setListOfCoins] = useState([])
-  const [searchedCoin,setSearchedCoin] = useState("")
-
-  const filteredCoin = listOfCoins.filter((coin)=>{
-    return coin.name.toLowerCase().includes(searchedCoin.toLocaleLowerCase())
-  })
+function App(){
+  const [listedCoins,setListedCoins] = useState([]);
+  const [searchedCoin,setSearchedCoin] = useState('');
 
   useEffect(()=>{
     axios.get('http://localhost:3000/coins')
-    .then((res)=>{
-      setListOfCoins(res.data)
+    .then(res=>{
+      setListedCoins(res.data);
     })
   },[])
-  return (
+
+  const filteredCoin = listedCoins.filter((coin)=>{
+    return coin.name.toLowerCase().includes(searchedCoin.toLowerCase())
+  })
+
+  return(
     <div className="App">
-    <div className="cryptoHeader">
-      <input type="text" placeholder="search for coins" className="cryptoInput" onChange={(event)=>{
-        setSearchedCoin(event.target.value)
-      }}/>
+      <div className="cryptoHeader">
+        <input type="text" placeholder="Search crypto name" className="cryptoInput"
+        onChange={(event)=>{
+          setSearchedCoin(event.target.value)
+        }}/>
+      </div>
+      <div className="cryptoData">
+        {filteredCoin.map((coin)=>{
+          return <Coin name={coin.name} icon={coin.icon} rank={coin.rank} symbol={coin.symbol} price={coin.price} />
+        })}
+      </div>
     </div>
-    <div className="cryptoData">
-    {filteredCoin.map((coin)=>{
-      return <Coin name={coin.name} icon={coin.icon} rank={coin.rank} symbol={coin.symbol} price={coin.price}/>
-    })}
-    </div>
-    </div>
-  );
+  )
 }
 
 export default App;
--- src/Coin.js
+++ src/Coin.js
@@ -1,14 +1,14 @@
 import React from "react";
-import './style.css'
+import './style.css';
 
 function Coin({name,icon,rank,symbol,price}){
     return(
         <div className="Coin">
-        <h1>Name:{name}</h1>
-        <img src={icon} alt=""/>
-        <h3>Rank:{rank}</h3>
-        <h3>Symbol:{symbol} </h3>
-        <h3>price:{price}</h3>
+            <h1>Name:{name}</h1>
+            <img src={icon} alt="" />
+            <h3>Rank:{rank}</h3>
+            <h3>Symbol:{symbol}</h3>
+            <h3>Price:{price}</h3>
         </div>
     )
 }
